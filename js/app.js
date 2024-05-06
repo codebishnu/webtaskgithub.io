6df4676a0fd8c911e5b009a444f2c32b0b1e1cf3
codebishnu <coderbishnu03@gmail.com>
2024-05-07 00:42:34 +0530

    webtask

---
+++ js/app.js
@@ -0,0 +1,12 @@
+
+// for menu btn 
+
+let navbar = document.querySelector('.header .navbar'); 
+
+document.querySelector('#menu-btn').onclick = () => {
+    navbar.classList.toggle('active');
+}
+
+window.onscroll = () => {
+    navbar.classList.remove('active');
+}
